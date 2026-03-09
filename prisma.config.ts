9c855440382520d823dfd9123ccf0da73a7b9a4d
Siraphat-ohm <siraphat975@gmail.com>
2026-03-09 10:06:08 +0700

    fix(prisma.config): update DATABASE_URL handling to use env function from prisma/config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "prisma/config";
-
+import "dotenv/config";
+import { defineConfig, env } from "prisma/config";
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
@@ -7,6 +7,6 @@ export default defineConfig({
     seed: "tsx prisma/seed.ts",
   },
   datasource: {
-    url: process.env.DATABASE_URL!,
+    url: env("DATABASE_URL"),
   },
 });
